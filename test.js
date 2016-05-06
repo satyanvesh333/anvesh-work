e941abe3315127d9eb61043cde83782b07656d2f
anvesh99899@gmail.com <anvesh99899@gmail.com>
2016-05-06 13:10:59 -0400

    Testing

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
+app.controller('myCtrl', function($scope, $http) {
+    $http.get("external.json").then(function (response) {
+        $scope.myWelcome = response.data;
+        $scope.firstName = "John";
+        $scope.lastName = "Doe";
+        
+    });
 });
-
